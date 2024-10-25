291b2bd8cee6d9cffc89d1391f34df15c62b48b1
nantha222 <krishnanantha02@gmail.com>
2024-10-25 23:38:18 +0530

    error fixed by nantha 25-10-24

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import SignUpPage from "./pages/SignUpPage"
-import LoginPage from "./pages/LoginPage"
-import EmailVerificationPage from "./pages/EmailVerificationPage"
-import ResetPasswordPage from "./pages/ResetPagePassword"
-import ForgotPasswordPage from "./pages/ForgetPasswordPage"
-import Home from "./pages/Home"
+import React from "react";
+import { Routes, Route } from "react-router-dom"; 
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage"; 
+import SignUpPage from "./pages/SignUpPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import ResetPagePassword from "./pages/ResetPagePassword"; 
+import ForgetPasswordPage from "./pages/ForgetPasswordPage"; 
+
 function App() {
-   return (
-
-     <div className="min-h-screen bg-gradient-to-br 
-     from-gray-900
-     via-green-900 
-     to-emerald-900 
-     ">
-     
-     
-    <Routes>
-
-<<<<<<< HEAD
-      <Route path="/home" element={<Home/>}/>
-=======
-      <Route path="/" element={<LoginPage/>}/>
->>>>>>> 45767d9edc9a12afd854e5969b06ba1cb3d6d6b0
-      <Route path="/signup" element={<SignUpPage/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/emailverfication" element={<EmailVerificationPage/>}/>
-      <Route path="/reset-password" element={<ResetPasswordPage/>}/>
-      <Route path="/forget-password" element={<ForgotPasswordPage/>}/>
-
-
-
-    </Routes>
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/emailverification" element={<EmailVerificationPage />} />
+        <Route path="/reset-password" element={<ResetPagePassword />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} /> 
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
